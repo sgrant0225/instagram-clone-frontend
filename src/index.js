c6ca2833397485542d3cc40100bb97ddfcff8b92
github username <sharnellgrant@gmail.com>
2022-01-11 00:47:40 -0500

    setup the index, app files, imported libraries

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'react-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import App from './component/App'
+
+//create store function
+const store = createStore(
+    rootReducer, 
+    composeWithDevTools(applyMiddleware(thunk))
+    )
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)    
